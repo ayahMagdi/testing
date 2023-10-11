886c46def9bf480a0ddad8c830b2d6a35f787ea8
ayahMagdi <ana.magdy321@gmail.com>
2023-10-11 03:30:28 +0300

    edit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
-import logoImg from '../assets/logo.jpg'
+// import logoImg from '../assets/logo.jpg'
+import logoImg from '../assets/Frame 48095522.png'
 import Form from '../components/Form'
 
 const Login = () => {
@@ -8,12 +9,12 @@ const Login = () => {
         <div className="flex justify-center items-center text-center w-7/12">
             <Form />
         </div>
-        <div className='hidden lg:flex bg-white justify-center items-center relative w-5/12'>
-          <div className='absolute w-full h-full bg-main rounded-full scale-x-[1.7] scale-y-[1.4] rotate-90 z-1 -left-36'></div>
-          <div className='z-2 relative flex justify-center items-center flex-col'>
-            <img src={logoImg} alt='logo' className='w-2/4' />
-            <h2 className="text-6xl font-bold text-white text-center mt-4">Logo</h2>
-          </div>
+        <div className='hidden lg:flex justify-start items-center relative w-5/12'>
+          {/* <div className='absolute w-full h-full  rounded-full scale-x-[1.7] scale-y-[1.4] rotate-90 z-1 -left-36'></div> */}
+          {/* <div className='z-2 relative flex justify-center items-center flex-col'> */}
+            <img src={logoImg} alt='logo' className='w-full' />
+            {/* <h2 className="text-6xl font-bold text-white text-center mt-4">Logo</h2> */}
+          {/* </div> */}
         </div>
     </div>
   )
